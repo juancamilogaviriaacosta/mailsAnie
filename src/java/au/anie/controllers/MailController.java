efea809dea3a1bc8cd1bd8389719957c9e0818e9
juan <juancamilogaviriaacosta@gmail.com>
2019-11-28 15:58:52 -0500

    mail

--- src/java/au/anie/controllers/MailController.java
+++ src/java/au/anie/controllers/MailController.java
@@ -5,13 +5,28 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -38,56 +53,64 @@ public class MailController {
 
     public void print() {
         try {
-            Map<String, Object> parametros = new HashMap<>();
-            parametros.put("name", "Juan");
-            parametros.put("adress1", "");
-            parametros.put("adress2", "");
-            parametros.put("attendance", "");
-            parametros.put("date", "");
-
-            String path = new File(this.getClass().getResource("MailController.class").getPath()).getParent() + File.separator + "mail.jasper";
-            JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(path);
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, new JREmptyDataSource());
-            JasperExportManager.exportReportToPdfStream(jasperPrint, new FileOutputStream("/home/juan/Escritorio/students.pdf"));
-
             File file = new File("/home/juan/Escritorio/students.xlsx");
-            List<List<List<Object>>> archivoExcel = getArchivoExcel(new FileInputStream(file), file.getName());
-            for (List<List<Object>> hoja : archivoExcel) {
-                for (List<Object> fila : hoja) {
-                    for (Object celda : fila) {
-                        System.out.println(celda);
-                    }
-                }
+            FileInputStream fis = new FileInputStream(file);
+            Workbook workbook = file.getName().endsWith(".xls") ? new HSSFWorkbook(fis) : new XSSFWorkbook(fis);
+            DataFormatter formatter = new DataFormatter();
+
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.next(); //skip header
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+
+                Map<String, Object> parametros = new HashMap<>();
+                parametros.put("name", formatter.formatCellValue(row.createCell(0)));
+                parametros.put("adress1", formatter.formatCellValue(row.createCell(1)));
+                parametros.put("adress2", formatter.formatCellValue(row.createCell(2)));
+                parametros.put("attendance", formatter.formatCellValue(row.createCell(3)));
+                parametros.put("date", formatter.formatCellValue(row.createCell(4)));
+                String mail = formatter.formatCellValue(row.createCell(5));
+
+                String path = new File(this.getClass().getResource("MailController.class").getPath()).getParent() + File.separator + "mail.jasper";
+                JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(path);
+                JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, new JREmptyDataSource());
+                JasperExportManager.exportReportToPdfStream(jasperPrint, new FileOutputStream("/home/juan/Escritorio/students.pdf"));
+
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static List<List<List<Object>>> getArchivoExcel(InputStream fis, String nombre) throws IOException {
-        Workbook workbook = nombre.endsWith(".xls") ? new HSSFWorkbook(fis) : new XSSFWorkbook(fis);
-        List<List<List<Object>>> resp = new ArrayList<>();
-        DataFormatter formatter = new DataFormatter();
+    public void sendAttachmentEmail(Session session, String toEmail, String subject, String body) throws MessagingException, UnsupportedEncodingException {
 
-        int n = workbook.getNumberOfSheets();
-        for (int i = 0; i < n; i++) {
-            List<List<Object>> hoja = new ArrayList<>();
-            Sheet sheet = workbook.getSheetAt(i);
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                List<Object> fila = new ArrayList<>();
-                Row row = rowIterator.next();
-                Iterator<Cell> cellIterator = row.cellIterator();
-                while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-                    Object valor = formatter.formatCellValue(cell);
-                    fila.add(valor);
-                }
-                hoja.add(fila);
-            }
-            resp.add(hoja);
-        }
-        return resp;
+        MimeMessage msg = new MimeMessage(session);
+        msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+        msg.addHeader("format", "flowed");
+        msg.addHeader("Content-Transfer-Encoding", "8bit");
+        msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
+        msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+        msg.setSubject(subject, "UTF-8");
+        msg.setSentDate(new Date());
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+        BodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setText(body);
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+
+        messageBodyPart = new MimeBodyPart();
+        String filename = "abc.txt";
+        DataSource source = new FileDataSource(filename);
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName(filename);
+        multipart.addBodyPart(messageBodyPart);
+
+        msg.setContent(multipart);
+
+        Transport.send(msg);
     }
 
     public String getName() {
